e87b3f4f6fdae7a0731365ccbedce4bc8af284ba
Daniel Vargas <dannyvargashb@gmail.com>
2020-06-22 17:59:00 -0700

    Modifications to add images to render

--- app.js
+++ app.js
@@ -1,6 +1,13 @@
 let tbody = document.getElementById("tableBody");
 let search = document.getElementById("search");
 const buttonParent = document.getElementById("btns");
+let map = null;
+function initMap() {
+  map = new google.maps.Map(document.getElementById("map"), {
+    center: { lat: -34.397, lng: 150.644 },
+    zoom: 8
+  });
+}
 
 search.addEventListener("click", getBeers);
 
@@ -20,14 +27,22 @@ function getBeers(e) {
         let beerAbv = document.createElement('td');
         let beerIbu = document.createElement('td');
         let beerDescription = document.createElement('td');
+        let beerImage = document.createElement('td');
 
 
         beerName.textContent = data[i].name;
         beerAbv.textContent = data[i].abv;
         beerIbu.textContent = data[i].ibu;
         beerDescription.textContent = data[i].description;
+        beerImage.style.backgroundImage = `url(${data[i].image_url})`;
+        beerImage.style.backgroundPosition = "center";
+        beerImage.style.backgroundSize = "25px 60px";
+        beerImage.style.backgroundRepeat = "no-repeat";
+        if(data[i].image_url === null) {
+          beerImage.textContent = "N/A";
+        }
 
-        row.append(beerName, beerAbv, beerIbu, beerDescription);
+        row.append(beerName, beerAbv, beerIbu, beerDescription, beerImage);
         tbody.append(row);
       }
       search.removeEventListener('click', getBeers);
